f15f15f043c957e3f56593332ea36d2551c674c8
Sai Charan Veeranki <scv.learn@gmail.com>
2025-03-21 08:54:42 +0530

    Initial Commit

---
+++ VariablesDataTypes/Variables.py
@@ -0,0 +1,25 @@
+# a = 10
+# b = 20
+
+# print("Addition of a and b :",a+b)
+
+
+# message = "Veeranki sai charan"
+# print(len(message))
+
+# message = "Veeranki Sai Charan"
+# capital_text = message.upper()
+# print(capital_text)
+
+# message = "VEERANKI SAI CHARAN"
+# lower_string = message.lower()
+# print(lower_string)
+
+# text  = "My name is sai charan and i am from vijayawada"
+# print(text.find("VIJAYAWADA"))
+# print("VIJAYAWADA" in text)
+
+
+sentence = "I love java and i have a good knowledge in java"
+modified_sentence = sentence.replace("java","python")
+print(modified_sentence)
